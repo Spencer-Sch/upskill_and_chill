5912253045da36ea59636d72b416a0fa6939f8c5
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-04-19 13:38:00 -0500

    add onchange handle

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,21 +1,44 @@
-'use client'
+"use client";
+import React, { useState, ChangeEvent } from "react";
 
 const Todo = () => {
+  const [taskData, setTaskData] = useState({ taskName: "", taskBody: "" });
+
+  // Start next stream talking about updated event type
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTaskData((prev) => ({ ...prev, taskBody: e.target.value }));
+  };
+  // console.log("handleChange: ", taskData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
       This works
-      <form onSubmit={(e) => {
-        e.preventDefault()
-      }}>
-        <input 
-          type="text"
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="task-name">Task Name</label>
+        <input
+          type="text-name"
+          name="task-name"
+          id="task-name"
           className="border-[1px]"
+        />
+        <label htmlFor="task-body">Task Body</label>
+        <input
+          type="task-body"
+          name="task-body"
+          id="task-body"
+          className="border-[1px]"
+          onChange={handleChange}
+          value={taskData.taskBody}
         />
         <button type="submit">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
+
