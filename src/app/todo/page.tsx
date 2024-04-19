3d3cbc6425cdf720de48c7988962cdd217deefa0
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-04-19 10:37:34 -0700

    added use client tag and onSubmit behavior to form

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 const Todo = () => {
 
   return (
     <div>
       This works
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+      }}>
         <input 
           type="text"
           className="border-[1px]"
