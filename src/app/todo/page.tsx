6c6f4ff4209b647ca3553f74802f851a728ae413
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-04-26 10:37:40 -0700

    created prettierrc file and enforced formatting

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,44 +1,43 @@
-"use client";
-import React, { useState, ChangeEvent } from "react";
+'use client'
+import React, { useState, ChangeEvent } from 'react'
 
 const Todo = () => {
-  const [taskData, setTaskData] = useState({ taskName: "", taskBody: "" });
+	const [taskData, setTaskData] = useState({ taskName: '', taskBody: '' })
 
-  // Start next stream talking about updated event type
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTaskData((prev) => ({ ...prev, taskBody: e.target.value }));
-  };
-  // console.log("handleChange: ", taskData);
+	// Start next stream talking about updated event type
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setTaskData((prev) => ({ ...prev, taskBody: e.target.value }))
+	}
+	// console.log("handleChange: ", taskData);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+	const handleSubmit = (e: any) => {
+		e.preventDefault()
+	}
 
-  return (
-    <div>
-      This works
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="task-name">Task Name</label>
-        <input
-          type="text-name"
-          name="task-name"
-          id="task-name"
-          className="border-[1px]"
-        />
-        <label htmlFor="task-body">Task Body</label>
-        <input
-          type="task-body"
-          name="task-body"
-          id="task-body"
-          className="border-[1px]"
-          onChange={handleChange}
-          value={taskData.taskBody}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+	return (
+		<div>
+			This works
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="task-name">Task Name</label>
+				<input
+					type="text-name"
+					name="task-name"
+					id="task-name"
+					className="border-[1px]"
+				/>
+				<label htmlFor="task-body">Task Body</label>
+				<input
+					type="task-body"
+					name="task-body"
+					id="task-body"
+					className="border-[1px]"
+					onChange={handleChange}
+					value={taskData.taskBody}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	)
+}
 
-export default Todo;
-
+export default Todo
