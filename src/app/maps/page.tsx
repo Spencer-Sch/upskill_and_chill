d06fa687ae1db71795dcf8228a47390c37489377
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-05-17 14:00:53 -0500

    add Markers to GoogleMap

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -2,6 +2,12 @@
 import React from 'react'
 import GoogleMap from '../components/GoogleMap'
 import { MapProvider } from '../hooks/MapProvider'
+import Marker from '../components/Marker'
+
+const locations = [
+	{ lat: 44.938255265678244, lng: -93.11162606015404 },
+	{ lat: 44.83623033897452, lng: -92.95792745396419 },
+]
 
 const Maps = () => {
 	// use fetch() to pull Random User Data
@@ -9,10 +15,11 @@ const Maps = () => {
 	// pass to GoogleMap
 	return (
 		<MapProvider mapApiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY as string}>
-			<GoogleMap
-				zoom={12}
-				center={{ lat: 44.980553, lng: -93.270035 }}
-			></GoogleMap>
+			<GoogleMap zoom={12} center={{ lat: 44.980553, lng: -93.270035 }}>
+				{locations.map((location, idx) => {
+					return <Marker key={idx} position={location} />
+				})}
+			</GoogleMap>
 		</MapProvider>
 	)
 }
