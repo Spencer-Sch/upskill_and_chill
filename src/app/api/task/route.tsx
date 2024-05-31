a28018f61088b35c45817209c39d4ef0f7299582
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-05-31 12:54:57 -0700

    reorganized file structure to include prisma inside of /src

--- src/app/api/task/route.tsx
+++ src/app/api/task/route.tsx
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '../../../../prisma/client'
-// import { prisma } from '../../../../prisma/client'
+import { prisma } from '@/prisma/client'
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
