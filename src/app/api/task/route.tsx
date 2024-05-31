e38cb5af25938443b6d45e27ce8708434a3a6eca
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-05-31 11:04:52 -0700

    created api folder and first task endpoint

---
+++ src/app/api/task/route.tsx
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '../../../../prisma/client'
+// import { prisma } from '../../../../prisma/client'
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  const task = await prisma.task.create({
+    data: {
+      ...body
+    }
+  })
+  return NextResponse.json(task, { status: 201 })
+}
