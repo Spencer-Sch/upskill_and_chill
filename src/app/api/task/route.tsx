59403cc358bce08c1c861232014ff1e968604b73
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-06-07 12:13:16 -0500

    formatting

--- src/app/api/task/route.tsx
+++ src/app/api/task/route.tsx
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/prisma/client'
 
 export async function POST(request: NextRequest) {
-  const body = await request.json()
-  const task = await prisma.task.create({
-    data: {
-      ...body
-    }
-  })
-  return NextResponse.json(task, { status: 201 })
+	const body = await request.json()
+	const task = await prisma.task.create({
+		data: {
+			...body,
+		},
+	})
+	return NextResponse.json(task, { status: 201 })
 }
