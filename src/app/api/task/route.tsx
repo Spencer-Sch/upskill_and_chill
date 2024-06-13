9814346d19ada41753e7ac038e6c4b8496985b38
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-13 12:28:07 -0700

    added uuid for id generation

--- src/app/api/task/route.tsx
+++ src/app/api/task/route.tsx
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/prisma/client'
+// import { v4 } from 'uuid'
+import { randomUUID } from 'crypto'
 
 export async function POST(request: NextRequest) {
 	const body = await request.json()
 	const createdAt = Date.now().toString()
+	const id = randomUUID()
 	const task = await prisma.task.create({
 		data: {
 			...body,
