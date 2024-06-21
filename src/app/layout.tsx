7fa4a305c87fd00e6ec896253dbf0bc60524fc7f
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-21 11:22:28 -0700

    styling work on form and task list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-primaryBg`}>{children}</body>
+    <html lang="en" className="h-full w-full">
+      <body className={`${inter.className} h-full w-full bg-primaryBg`}>{children}</body>
     </html>
   );
 }
