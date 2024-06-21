a7554f89b3fba3f4621abdbfd84ea16976b3f8c8
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-06-21 10:35:28 -0700

    set custom colors, changed bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,15 @@ const config: Config = {
   ],
   theme: {
     colors: {
-      'primary': ''
+      vanilla: '#FBE8A6',
+      orange: '#F4976C',
+      darkBlue: '#303C6C',
+      lightBlue: '#B4DFE5',
+      white: '#D2FDFF',
+      primary: '#303C6C',
+      secondary: '#F4976C',
+      tertiary: '#B4DFE5',
+      primaryBg: '#FBE8A6'
     },
     extend: {
       backgroundImage: {
