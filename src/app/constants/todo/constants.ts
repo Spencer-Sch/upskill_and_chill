3e75ba0c022451af1f406b48eeedbf36121e2d92
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-07-02 14:10:40 -0500

    add constants file

---
+++ src/app/constants/todo/constants.ts
@@ -0,0 +1,20 @@
+export const priorityOptions = [
+	{
+		label: 'High',
+		value: 3,
+	},
+	{
+		label: 'Medium',
+		value: 2,
+	},
+	{
+		label: 'Low',
+		value: 1,
+	},
+]
+
+export const priorityOptionsMap: { [key: number]: string } = {
+	1: 'Low',
+	2: 'Medium',
+	3: 'High',
+}
