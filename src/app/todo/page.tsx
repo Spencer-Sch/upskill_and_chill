b9fe9cb352f1338be2a902bd5f69db0335165c0b
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-09-06 15:03:42 -0500

    add max height and vertical scroll to list of tasks

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -130,7 +130,7 @@ const Todo = async () => {
 				</form>
 			</div>
 			<div className="p-10">
-				<ul className="space-y-3">
+				<ul className="space-y-3 max-h-[700px] overflow-y-scroll pr-5">
 					<Suspense fallback={<div>Loading...</div>}>
 						{tasks.map((task: Task) => (
 							<Task key={task.id} taskData={task} />
