16008b97e18c026f35f3578638f228b175bcbfb5
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-10-04 11:41:49 -0700

    imported task component and created loader for tasks

--- remix/todo-app/app/routes/todo.tsx
+++ remix/todo-app/app/routes/todo.tsx
@@ -1,4 +1,17 @@
+import { Suspense } from 'react'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { prisma } from '../../prisma/client'
+import Task from '../components/Task'
+
+
+export const loader = async () => {
+  const tasks: Task[] = await prisma.task.findMany()
+  return json(tasks)
+}
+
 export default function Todo() {
+  const tasks = useLoaderData<typeof loader>()
   return (
     <div className="bg-primary-400 flex space-x-10 w-fit rounded-md">
 			<div className="p-10">
@@ -6,11 +19,11 @@ export default function Todo() {
 			</div>
 			<div className="p-10">
 				<ul className="space-y-3 max-h-[700px] overflow-y-scroll pr-5">
-					{/* <Suspense fallback={<div>Loading...</div>}>
-						{tasks.map((task: Task) => (
+					<Suspense fallback={<div>Loading...</div>}>
+						{tasks.map(task => (
 							<Task key={task.id} taskData={task} />
 						))}
-					</Suspense> */}
+					</Suspense>
 				</ul>
 			</div>
 		</div>
