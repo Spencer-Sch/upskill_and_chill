fbe3eb1351323d7f000e40768ec45c672a69e207
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-10-18 15:04:07 -0500

    add IconButton

---
+++ remix/todo-app/app/components/IconButton.tsx
@@ -0,0 +1,31 @@
+import { ReactElement, ReactNode } from "react";
+
+interface ButtonProps {
+  style?: "primay" | "secondary";
+  onClick?: (...args: any[]) => void;
+  label?: string;
+  ariaLabel: string;
+  icon: ReactElement;
+  iconPosition?: "left" | "right";
+}
+
+export default function Button({
+  style,
+  onClick,
+  label,
+  ariaLabel,
+  icon,
+  iconPosition = "left",
+}: ButtonProps) {
+  return (
+    <button
+      className="flex justify-center items-center space-x-2 p-1.5 rounded-md transition-colors duration-200 hover:bg-red-500 hover:bg-opacity-80 hover:text-white"
+      aria-label={ariaLabel}
+      onClick={onClick ? onClick : () => {}}
+    >
+      {iconPosition === "left" && icon}
+      {label && <span>{label}</span>}
+      {iconPosition === "right" && icon}
+    </button>
+  );
+}
