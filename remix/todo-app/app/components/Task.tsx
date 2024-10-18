c7b775a99b87ca319243f6de433b2f65f3385275
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-10-18 15:06:09 -0500

    bring in trash icon, IconButton, use fetcher.Form

--- remix/todo-app/app/components/Task.tsx
+++ remix/todo-app/app/components/Task.tsx
@@ -1,42 +1,46 @@
+import { useFetcher } from "@remix-run/react";
 // import { format } from 'date-fns'
-// import { Trash } from '../third-party/icons'
-// import IconButton from './IconButton'
-// import { deleteTask } from '../../lib/actions'
+import { Trash } from "@phosphor-icons/react";
+import IconButton from "~/components/IconButton";
 
 interface TaskProps {
-	taskData: Task
+  taskData: Task;
 }
 
 const Task = ({
-	taskData: { taskName, description, id, priority, completed, createdAt },
+  taskData: { taskName, description, id, priority, completed, createdAt },
 }: TaskProps) => {
-	const getPriorityColor = (priority: number) => {
-		const map: { [key: number]: string } = {
-			3: 'border-red-500',
-			2: 'border-yellow-500',
-			1: 'border-grey-500',
-		}
-		return map[priority]
-	}
-	return (
-		<li
-			className={`${getPriorityColor(
-				priority
-			)} max-w-[350px] min-w-[350px] p-5 bg-lightBlue rounded-md border-l-4 border-solid`}
-		>
-			<h3 className="text-lg break-words">{taskName}</h3>
-			<p>{description}</p>
-			<div className="flex justify-between items-center w-full mt-3">
-				<p className="text-grey-500 italic">
-					{/* {format(new Date(Number(createdAt)) ?? '', 'MM/dd/yyyy')} */}
-				</p>
-				{/* <form action={deleteTask}>
-					<input hidden defaultValue={id ?? ''} name="taskId" />
-					<IconButton icon={<Trash size={17} />} ariaLabel="delete task" />
-				</form> */}
-			</div>
-		</li>
-	)
-}
+  const fetcher = useFetcher();
+  const getPriorityColor = (priority: number) => {
+    const map: { [key: number]: string } = {
+      3: "border-red-500",
+      2: "border-yellow-500",
+      1: "border-grey-500",
+    };
+    return map[priority];
+  };
+  return (
+    <li
+      className={`${getPriorityColor(
+        priority
+      )} max-w-[350px] min-w-[350px] p-5 bg-lightBlue rounded-md border-l-4 border-solid`}
+    >
+      <h3 className="text-lg break-words">{taskName}</h3>
+      <p>{description}</p>
+      <div className="flex justify-between items-center w-full mt-3">
+        <p className="text-grey-500 italic">
+          {/* {format(new Date(Number(createdAt)) ?? '', 'MM/dd/yyyy')} */}
+        </p>
+        {/* <form action={deleteTask}> */}
+        <fetcher.Form method="delete">
+          <input hidden defaultValue={id ?? ""} name="taskId" />
 
-export default Task
+          <IconButton icon={<Trash size={17} />} ariaLabel="delete task" />
+        </fetcher.Form>
+        {/* </form> */}
+      </div>
+    </li>
+  );
+};
+
+export default Task;
