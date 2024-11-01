adb5fefbb1e3cd428e5478448307c667bab8e9cb
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-11-01 14:44:55 -0500

    add remix-auth, login page, move some form methods around between route actions and resource routes

--- remix/todo-app/app/routes/api.todo.tsx
+++ remix/todo-app/app/routes/api.todo.tsx
@@ -1,9 +1,13 @@
-import { json } from "@remix-run/node";
-import { prisma } from "../../prisma/client";
+import { ActionFunctionArgs } from "@remix-run/node";
+// import { prisma } from "../../prisma/client";
+import { createTask } from "~/actions/todo-actions";
 
+// export const loader = async () => {
+//   const tasks: Task[] = await prisma.task.findMany();
+//   return json(tasks);
+// };
 
-export const loader = async () => {
-  const tasks: Task[] = await prisma.task.findMany();
-  return json(tasks);
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  return createTask(formData);
 };
-
