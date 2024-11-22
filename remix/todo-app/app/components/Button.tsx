1c28af15827c6e3e9dccf8be5095067900717042
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-11-22 12:03:25 -0600

    add styles and fullWidth prop to Button

--- remix/todo-app/app/components/Button.tsx
+++ remix/todo-app/app/components/Button.tsx
@@ -1,38 +1,40 @@
-'use client'
-import { ReactElement, ReactNode } from 'react'
+"use client";
+import { ReactElement, ReactNode } from "react";
 
 interface ButtonProps {
-	style?: 'primay' | 'secondary'
-	onClick?: (...args: any[]) => void
-	type?: 'button' | 'submit' | 'reset' | undefined
-	label?: string
-	icon?: ReactElement | null
-	iconPosition?: 'left' | 'right'
+  style?: "primay" | "secondary";
+  onClick?: (...args: any[]) => void;
+  type?: "button" | "submit" | "reset" | undefined;
+  label?: string;
+  icon?: ReactElement | null;
+  fullWidth?: boolean;
+  iconPosition?: "left" | "right";
 }
 
-type Field = { [key: string]: any }
+type Field = { [key: string]: any };
 
-type OtherProps = ButtonProps & Field
+type OtherProps = ButtonProps & Field;
 
 export default function Button({
-	style,
-	onClick,
-	type,
-	label,
-	icon,
-	iconPosition = 'left',
-	...props
+  style,
+  onClick,
+  type,
+  label,
+  icon,
+  fullWidth,
+  iconPosition = "left",
+  ...props
 }: OtherProps) {
-	return (
-		<button
-			type={type}
-			className="flex justify-center items-center space-x-2 hover:bg-secondary-600 py-2 rounded-md bg-secondary-500"
-			onClick={onClick ? onClick : () => {}}
-			{...props}
-		>
-			{iconPosition === 'left' && icon}
-			<span>{label}</span>
-			{iconPosition === 'right' && icon}
-		</button>
-	)
+  return (
+    <button
+      type={type}
+      className={`button ${fullWidth ? "w-full" : ""}`}
+      onClick={onClick ? onClick : () => {}}
+      {...props}
+    >
+      {iconPosition === "left" && icon}
+      <span>{label}</span>
+      {iconPosition === "right" && icon}
+    </button>
+  );
 }
