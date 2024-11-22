24c9b8526e59f1e7bc8d8340e9c19a6c091788c1
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-11-22 12:10:40 -0600

    updates to todo: remove logout, remove commented code, etc

--- remix/todo-app/app/routes/todo.tsx
+++ remix/todo-app/app/routes/todo.tsx
@@ -1,44 +1,31 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import { json } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
-import { prisma } from "../../prisma/client";
+import { useLoaderData, useNavigation } from "@remix-run/react";
+// import { prisma } from "../../prisma/client";
 import Task from "../components/Task";
-import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import TodoForm from "../components/TodoForm";
-import { createTask, deleteTask } from "~/actions/todo-actions";
 import { authenticator } from "~/services/auth.server";
-import { supabase } from '../../supabase/client'
-import { Tables } from '../../database.types'
-
-// export async function loader({ request }: LoaderFunctionArgs) {
-// 	// If the user is already authenticated redirect to /dashboard directly
-// 	const user = await authenticator.isAuthenticated(request, {
-// 		failureRedirect: '/login',
-// 	})
-// 	console.log('user - dashboard: ', user)
-// 	return user
-// }
+import { supabase } from "../../supabase/client";
+import { Tables } from "../../database.types";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const user = await authenticator.isAuthenticated(request, {
+  await authenticator.isAuthenticated(request, {
     failureRedirect: "/login",
   });
 
   // const tasks: Task[] = await prisma.task.findMany();
-  const { data, error } = await supabase
-    .from('tasks')
-    .select()
-  if (error) throw error
-  const tasks: Tables<'tasks'>[] = data
+  // return tasks;
+  const { data, error } = await supabase.from("tasks").select();
+  if (error) throw error;
+  const tasks: Tables<"tasks">[] = data;
   return json(tasks);
 };
 
-export async function action({ request }: ActionFunctionArgs) {
-  await authenticator.logout(request, { redirectTo: "/login" });
-}
-
 export default function Todo() {
   const tasks = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+
   return (
     <>
       <div className="bg-primary-400 flex space-x-10 w-fit rounded-md">
