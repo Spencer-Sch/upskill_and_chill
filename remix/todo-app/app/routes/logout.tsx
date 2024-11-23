1c8f68c129b2a7fd44dfeca13a39bb66717ee1a5
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2024-11-23 15:06:18 -0600

    add new logout route and aim logout form action to route

---
+++ remix/todo-app/app/routes/logout.tsx
@@ -0,0 +1,17 @@
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import {
+  getSession,
+  sessionStorage,
+  supabase,
+} from "~/services/session.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const session = await getSession(request);
+  await supabase.auth.signOut();
+
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await sessionStorage.destroySession(session),
+    },
+  });
+};
